ef44f7968cbf085a2fceeec6a993dac18ed49bce
Amanda Kellen <49374780+Amandakellen@users.noreply.github.com>
2020-10-21 14:24:06 -0300

    Iniciando leitura dos dados

--- Calculo_de_Probabilidade_de_falha.py
+++ Calculo_de_Probabilidade_de_falha.py
@@ -1,2 +1,22 @@
+import csv
 
+#Abrindo arquivos   CSV
+arquivo_cluster=open('ClusterDB.csv')
+arquivo_manutencao=open('MPDB.csv')
 
+cluster=csv.reader(arquivo_cluster)
+manutencao=csv.reader(arquivo_manutencao)
+
+linha=0
+coluna=0
+matrizcluster = [ [0 for i in range(3)] for j in range(4)]
+for row in cluster:
+    coluna=0
+    while coluna!=3:
+        matrizcluster[linha][coluna] = float(row[coluna])
+        coluna=coluna+1
+    linha=linha+1
+
+#Fechando arquivos
+arquivo_cluster.close()
+arquivo_manutencao.close()
